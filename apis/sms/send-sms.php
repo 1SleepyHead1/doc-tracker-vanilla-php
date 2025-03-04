6b0debf874a66227fdccbeb496da1d4061552fdf
1SleepyHead1 <peramanrogerp@gmail.com>
2025-03-04 15:03:47 +0800

    new commit 03-04-25

--- apis/sms/send-sms.php
+++ apis/sms/send-sms.php
@@ -5,8 +5,8 @@ require_once "../../script/globals.php";
 use Twilio\Rest\Client;
 
 try {
-    $sid    = "AC32b99d0b0d51fccfa277baca1c54c646";
-    $token  = "5e829381d82f1391a1c23fea90576781";
+    $sid    = "";
+    $token  = "";
     $twilio = new Client($sid, $token);
     $message = $twilio->messages
         ->create(
